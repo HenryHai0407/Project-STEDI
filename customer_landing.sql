13555eba002e8c3111d04f62203f24314bea68eb
HenryHai0407 <87645090+HenryHai0407@users.noreply.github.com>
2023-06-29 12:53:45 +0300

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS customer_landing (
+  customerName STRING,
+  email STRING,
+  phone STRING,
+  birthDay STRING,
+  serialNumber STRING,
+  registrationDate BIGINT,
+  lastUpdateDate BIGINT,
+  shareWithResearchAsOfDate BIGINT,
+  shareWithPublicAsOfDate BIGINT
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1'
+)
+LOCATION 's3://hainguyen/customer/landing/'
